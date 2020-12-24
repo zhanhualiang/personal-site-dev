afa8fceb490d08234b3b8033b67756c82cf59826
zhanhua_linag <593826071@qq.com>
2020-12-24 16:48:28 +0100

    add simple header and footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,12 @@
+<template>
+  <hr />
+  <footer id="footer">
+    <p>Copyright &copy; 2020 Zhanhua Liang. All right reserved</p>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'Footer'
+}
+</script>
---
+++ src/components/Header.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="header">
+    <h1>Zhanhua Liang</h1>
+    <p>Some call me Zhanhua, some call me Liang, and some call me "Cheng".</p>
+  </div>
+  <hr />
+</template>
+
+<script>
+export default {
+  name: 'Header'
+}
+</script>
+
+<style scoped>
+</style>

846af7cb13c30349beb0a32a7cd7a526ce4a5b0d
zhanhua_linag <593826071@qq.com>
2021-01-17 17:56:54 +0100

    uninstalled i18n and use 'add' instead

--- src/main.js
+++ src/main.js
@@ -3,5 +3,6 @@ import App from './App.vue'
 
 import './assets/global.css'
 import router from './router'
+import i18n from './i18n'
 
-createApp(App).use(router).mount('#app')
+createApp(App).use(router).use(i18n).mount('#app')
